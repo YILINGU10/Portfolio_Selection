a506658d23d98fd77e6340178ed21696317a2939
Leo Li <leo4life@163.com>
2019-09-08 11:38:12 +0800

    20190908 class

--- gym-ps/gym_ps/envs/ps_env.py
+++ gym-ps/gym_ps/envs/ps_env.py
@@ -23,11 +23,7 @@ class PSenv(gym.Env):
         All observations are assigned a uniform random value in [-0.05..0.05]
         
     Episode Termination:b
-        Pole Angle is more than 12 degrees
-        Cart Position is more than 2.4 (center of the cart reaches the edge of the display)
-        Episode length is greater than 200
-        Solved Requirements
-        Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
+        t >= length of prices
     """
     metadata = {'render.modes': ['human']}
 
@@ -36,30 +32,38 @@ class PSenv(gym.Env):
         self.t = 0
         self.t_prices = t_prices
 
-        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+        self.observation_space = spaces.Box(np.zeros(n_assets), high, dtype=np.float32)
         
         self.action_space = spaces.Box(np.zeros(n_assets),
-                                       np.ones(n_assets),
+                                       high,
                                        dtype=np.float32)
 
         self.state = t_prices[0]
 
     def step(self, action):
 #         assert all(np.sum(action, axis = 0) == 1), "Portfolio doesn't sum up to one"
+
+        action = np.clip(action, self.action_space.low, self.action_space.high)
+    
+        action /= np.sum(action)
+        
         t_price = self.t_prices[self.t]
     
         t_minus_1_prices = self.state
         
         t_returns = t_price / t_minus_1_prices # element wise division
+        
         self.state = t_price # update the state
         
         self.t += 1
         
         reward = np.dot(action.T, t_returns)
+        print('action is: ' + str(action))
+        print('reward is: ' + str(reward))
         
         done = self.t >= len(self.t_prices)
 
-        return np.array(self.state), done, reward, {}
+        return np.array(self.state), reward, done, {}
 
     def reset(self):
         self.t = 0
